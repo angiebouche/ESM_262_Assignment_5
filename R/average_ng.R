d2a3e296a771bf40fa396a723db53adb3724af21
Joslyn Fritz <joslynafritz@gmail.com>
2019-06-11 13:40:52 -0700

    Fixed package issue

--- R/average_ng.R
+++ R/average_ng.R
@@ -4,17 +4,16 @@
 #' @param data_ng is the data frame with all of the CO2 emissions
 #' @return value in tons of CO2
 
-ave_ng_year = function(data_ng) {
+average_ng = function(data_ng) {
 
    ng_emissions = data_ng %>%
     filter(Fuel_Type == "Natural Gas") %>%
     select(Total_CO2_Emissions) %>%
     summarize(mean = (sum(Total_CO2_Emissions)/7))
 
-   x = (data_ng = data)
 
 
-  return(result)
+  return(ng_emissions$mean)
 }
 
 
