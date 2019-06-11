a7f345f787fb5ff0627b138b3e314ee2caee4484
Angie Bouche <angiebouche@yahoo.com>
2019-06-11 15:58:35 -0700

    comit

--- R/average_ng.R
+++ R/average_ng.R
@@ -6,15 +6,13 @@
 #' @author Joslyn Fritz and Angie Bouche
 #' @example load in ghg dataset with fuel type, sector and year to compute average emissions
 
-average_ng.R = function(ghg_ng = ghg) {
+average_ng= function(ghg_ng = ghg) {
 
-   ng_emissions = data(ghg) %>%
+   ng_emissions = data(ghg) %>% 
     tidyr::filter(Fuel_Type == "Natural Gas") %>%
     tidyr::select(Total_CO2_Emissions) %>%
     dplyr::summarize(mean = (sum(Total_CO2_Emissions)/7))
-
-
-
+   
   return(ng_emissions$mean)
 }
 
