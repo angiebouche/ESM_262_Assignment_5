462105c99d892c79bd9f1a1ca0838fe50d05aaf6
Joslyn Fritz <joslynafritz@gmail.com>
2019-06-12 11:42:57 -0700

    impact functions

--- R/impact_function1.R
+++ R/impact_function1.R
@@ -1,21 +1,19 @@
-## Summarize: Average natural gas emissions by year
+## Calculate the impact of CO2 as it increases the warming of the planet. 
 #'
-#' compute net present value
-#' @param ghg_ng is the data frame with all of the CO2 emissions from natural gas
-#' @return value in tons of CO2
+#' @param ghg_ng is the data frame with all of the CO2 emissions from all fuel types and industries
+#' @return amount of planetary warming from CO2 emissions; for every 10 tons of CO2 emitted, the planet warms by 0.1 degrees Celsius. 
 #' @author Joslyn Fritz and Angie Bouche
-#' @example load in ghg dataset with fuel type, sector and year to compute average emissions
+#' @example load in ghg dataset with fuel type, to compute total emissions then divide by 10 and multiply by 0.2 to figure out the planetary warming from CO2 emissions. 
 
 average_ng_cco = function(ghg_data) {
   
-  ng_emissions = ghg_data %>%
-    dplyr::filter(Fuel_Type == "Natural Gas") %>%
+  global_warming = ghg_data %>%
     dplyr::select(Total_CO2_Emissions) %>%
-    dplyr::summarize(mean = (sum(Total_CO2_Emissions)/7))
+    dplyr::summarize(mean = (sum(Total_CO2_Emissions)/10)*0.1)
   
   
   
-  return(ng_emissions$mean)
+  return(global_warming$mean)
 }
 
 
