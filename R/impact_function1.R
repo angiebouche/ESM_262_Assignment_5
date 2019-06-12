f65d6805586204ed9259cf7b9e6cd5151d2585db
Joslyn Fritz <joslynafritz@gmail.com>
2019-06-12 10:50:30 -0700

    impact function

---
+++ R/impact_function1.R
@@ -0,0 +1,21 @@
+## Summarize: Average natural gas emissions by year
+#'
+#' compute net present value
+#' @param ghg_ng is the data frame with all of the CO2 emissions from natural gas
+#' @return value in tons of CO2
+#' @author Joslyn Fritz and Angie Bouche
+#' @example load in ghg dataset with fuel type, sector and year to compute average emissions
+
+average_ng_cco = function(ghg_data) {
+  
+  ng_emissions = ghg_data %>%
+    dplyr::filter(Fuel_Type == "Natural Gas") %>%
+    dplyr::select(Total_CO2_Emissions) %>%
+    dplyr::summarize(mean = (sum(Total_CO2_Emissions)/7))
+  
+  
+  
+  return(ng_emissions$mean)
+}
+
+
