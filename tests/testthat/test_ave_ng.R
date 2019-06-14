063b910cd4709c7bd9aa9966cb246c60952ec21e
Angie Bouche <angiebouche@yahoo.com>
2019-06-13 18:24:16 -0700

    test ng

--- tests/testthat/test_ave_ng.R
+++ tests/testthat/test_ave_ng.R
@@ -1,5 +1,6 @@
 test_that("Average natural gas emissions are numerical", {
   data(ghg)
-  expect_is(average_ng(data_ng = ghg), "numeric")
+  expect_is(average_ng(data_ghg = ghg), "numeric")
 })
+
 
